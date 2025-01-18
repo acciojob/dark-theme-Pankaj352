5869e39c63ecd476356c13c0724eb4bb01dcf5ff
Pankaj Maurya <100466858+Pankaj352@users.noreply.github.com>
2025-01-18 12:50:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 //your code here
 
+const swapbtn=document.getElementById('swap');
+const bgScreen=document.getElementById('app');
+
+
+swapbtn.addEventListener('click', ()=>{
+	bgScreen.classList.add('night');
+	swapbtn.classList.add('button_night');
+})
+
