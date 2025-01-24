0de79026ccb821274d9e5c3e0a9c77de2794c615
Pankaj Maurya <100466858+Pankaj352@users.noreply.github.com>
2025-01-24 23:55:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-//your code here
+const swapbtn = document.getElementById('swap');
+const bgScreen = document.getElementById('app');
 
-const swapbtn=document.getElementById('swap');
-const bgScreen=document.getElementById('app');
+swapbtn.addEventListener('click', () => {
+  
+  bgScreen.classList.toggle('night');
 
-
-swapbtn.addEventListener('click', ()=>{
-	bgScreen.classList.toggle('night');
-	// swapbtn.classList.remove('button_night');
-	// swapbtn.classList.remove('night');
-})
-
+  if (bgScreen.classList.contains('night')) {
+    swapbtn.classList.remove('button_day');
+    swapbtn.classList.add('button_night');
+  } else {
+    swapbtn.classList.remove('button_night');
+    swapbtn.classList.add('button_day');
+  }
+});
